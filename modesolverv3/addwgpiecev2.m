83e96fbe97a2313df851efd6093c3da7696c3119
Rishay Gupta <106123924+DrDevGupta@users.noreply.github.com>
2026-05-27 13:37:33 -0400

    Update addwgpiecev2.m

--- modesolverv3/addwgpiecev2.m
+++ modesolverv3/addwgpiecev2.m
@@ -1,7 +1,7 @@
 function [ermat] = addwgpiece(x1,y1,x2,y2,n,ermat,x,y)
 % adds piece to permittivity distribution
 % the points have their origin at the center of the distribution
-% Chris Doerr
+% Rishay Gupta
 
 ermat(x >= x1 & x <= x2, ...
     y >= y1 & y <= y2) = n^2; % set each coordinate's permittivity to n^2
