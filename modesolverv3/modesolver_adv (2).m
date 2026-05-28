146a4061c36253636d1ab86c00f0d473aceebf38
Rishay Gupta <106123924+DrDevGupta@users.noreply.github.com>
2026-05-28 10:24:12 -0400

    Update modesolver_adv (2).m
    included example of complex index

--- modesolverv3/modesolver_adv (2).m
+++ modesolverv3/modesolver_adv (2).m
@@ -12,7 +12,8 @@ wg.w = 0.5;         % waveguide width
 wg.h = 0.22;        % waveguide height
 ncore = 3.50;       % index of core
 nclad = 1.45;       % index of cladding
-neff_guess = 2.5;   % initial guess for effective index
+neff_guessRe = 2.5;   % initial guess for effective index
+neff_guessIm = 0.001;   % initial guess for loss component
 %% grid calculations
 x = unique([-xlim:0.04:-0.7, -0.7:0.01:0.7, 0.7:0.04:xlim]); % coarse spacing on the edges of the guide
 y = unique([-ylim:0.04:-0.4, -0.4:0.01:0.4, 0.4:0.04:ylim]); % dense spacing around the center of the guide
@@ -24,4 +25,4 @@ ermat = ones(Nx,Ny)*nclad^2;
 %---Si waveguide
 ermat = addwgpiece(-wg.w/2,-wg.h,wg.w/2,0,ncore,ermat,x,y);
 %% calculate modes
-[neff1,Ex1,Ey1,Ez1,Hx1,Hy1,Hz1] = modesolve_v2(lambdac,x,y,ermat,neff_guess,4,'TE',1);
+[neff1,Ex1,Ey1,Ez1,Hx1,Hy1,Hz1] = modesolve_v3(lambdac,x,y,ermat,neff_guessRe, neff_guessIm, 4,'TE',1);
